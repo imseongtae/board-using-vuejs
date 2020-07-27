10dcda0afd3ca2c6d2c9391c23020f5e35ec41ef
imseongtae <dev.haemil@gmail.com>
2020-07-27 21:33:12 +0900

    chore: Init board project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+	<div id="app">
+		<!-- main -->
+		<router-view />
+	</div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+// style 적용
+// @import 'http://localhost:8000/assets/index.css';
+@import './assets/reset.css';
 </style>
---
+++ src/pages/PostListPage.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="post-list-page">
+		<h1>포스트 게시글</h1>
+		<post-list :posts="posts" />
+	</div>
+</template>
+
+<script>
+import PostList from '@/components/PostList.vue';
+
+import api from '@/api';
+
+export default {
+	name: 'PostListPage',
+	components: {
+		PostList,
+	},
+	data() {
+		return {
+			posts: [],
+		};
+	},
+	created() {
+		api.get('/posts').then(res => {
+			console.log(res.data);
+			this.posts = res.data;
+		});
+	},
+};
+</script>
+
+<style lang="scss"></style>
