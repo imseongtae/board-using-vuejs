ed9fe36605ad1a6fd632ded7824d722816313a35
imseongtae <dev.haemil@gmail.com>
2020-07-27 22:26:18 +0900

    feat: Add Vuex instance in vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store'; // Vuex 인스턴스를 Vue 인스턴스에 store 옵션을 통해 주입
 
 Vue.config.productionTip = false;
 
 new Vue({
 	router,
+	store,
 	render: h => h(App),
 }).$mount('#app');
