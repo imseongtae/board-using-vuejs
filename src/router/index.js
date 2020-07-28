8a7e9fe58d8ef2716bb83c30dfd9371f9f471032
imseongtae <dev.haemil@gmail.com>
2020-07-29 00:06:22 +0900

    feat: Add Signin page route

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,11 @@ const routes = [
 		name: 'Signup',
 		component: () => import('@/pages/Signup.vue'),
 	},
+	{
+		path: '/signin',
+		name: 'Signin',
+		component: () => import('@/pages/Signin.vue'),
+	},
 	// route level code-splitting
 	// this generates a separate chunk (about.[hash].js) for this route
 	// which is lazy-loaded when the route is visited.
