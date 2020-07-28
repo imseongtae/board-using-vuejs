56b7f02ba997b922229e0542e47d4ae98304827e
imseongtae <dev.haemil@gmail.com>
2020-07-28 15:58:22 +0900

    feat: Add PostViewPage route

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,12 @@ const routes = [
 		name: 'PostListPage',
 		component: () => import('@/pages/PostListPage.vue'),
 	},
+	{
+		path: '/post/:postId',
+		name: 'PostViewPage',
+		props: true, // 컴포넌트의 props를 통해 라우트의 파라미터에 접근 설정
+		component: () => import('@/pages/PostViewPage.vue'),
+	},
 	// route level code-splitting
 	// this generates a separate chunk (about.[hash].js) for this route
 	// which is lazy-loaded when the route is visited.
