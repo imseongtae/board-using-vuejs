3373fa3dcf7cdeb706ad54558231e30983b7918c
imseongtae <dev.haemil@gmail.com>
2020-07-28 15:54:09 +0900

    feat: Add fetchPost action for execute FETCH_POST mutation and send data

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,16 +1,18 @@
 import api from '@/api';
-import { FETCH_POST_LIST } from './mutations-types';
+import { FETCH_POST_LIST, FETCH_POST } from './mutations-types';
 
+// API 서버와 통신을 통해 변이를 일으킬 액션
 export default {
 	fetchPostList({ commit }) {
-		// api.get('/posts').then(res => {
-		// 	console.log(res.data);
-		// 	this.posts = res.data;
-		// });
 		// 서버로부터 받아온 데이터를
 		// FETCH_POST_LIST 변이의 실행과 함께 인자로 넘겨줌
 		return api.get('/posts').then(res => {
 			commit(FETCH_POST_LIST, res.data);
+		});
+	},
+	fetchPost({ commit }, postId) {
+		return api.get(`/posts/${postId}`).then(res => {
+			commit(FETCH_POST, res.data);
 		});
 	},
 };
