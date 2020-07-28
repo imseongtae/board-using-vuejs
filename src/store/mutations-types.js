0c5b5fb603b757c186d8e2ee6cf4d7e53d30a8d4
imseongtae <dev.haemil@gmail.com>
2020-07-29 03:40:29 +0900

    feat: Add mutation type to add user info

--- src/store/mutations-types.js
+++ src/store/mutations-types.js
@@ -3,3 +3,6 @@ export const FETCH_POST_LIST = 'FETCH_POST_LIST';
 // 게시물 상세보기 페이지를 위한 상태 변이 타입을 선언
 export const FETCH_POST = 'FETCH_POST';
 export const SET_ACCESS_TOKEN = 'SET_ACCESS_TOKEN';
+
+// 사용자 정보를 추가하는 변이 타입 추가
+export const SET_MY_INFO = 'SET_MY_INFO';
