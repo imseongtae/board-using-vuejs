1c4c347de1f46d6a4353238aa32b4a0711ea6d89
imseongtae <dev.haemil@gmail.com>
2020-07-29 03:32:48 +0900

    feat: Use stored token in cookie

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store'; // Vuex 인스턴스를 Vue 인스턴스에 store 옵션을 통해 주입
+import Cookies from 'js-cookie';
 
 Vue.config.productionTip = false;
+
+// 쿠키에 저장된 토큰을 사용하여 인증
+// 쿠키에서 토큰을 검사하는 과정은 애플리케이션 초키화될 때 필요하므로 main.js 파일에 작성
+const savedToken = Cookies.get('accessToken');
+if (savedToken) {
+	store.dispatch('signinByToken', savedToken);
+}
 
 new Vue({
 	router,
