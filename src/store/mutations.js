98c8dadd9ac292eb62f0f819496b8ceb9de32908
imseongtae <dev.haemil@gmail.com>
2020-07-28 15:41:10 +0900

    feat: Add function for FETCH_POST mutation type

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,9 +1,12 @@
 // 추가한 스토어의 값을 변경할 수 있는 변이(Mutation) 추가
 // mutations-types 에 선언한 변이 이름을 불러와서 사용하도록 함
-import { FETCH_POST_LIST } from './mutations-types';
+import { FETCH_POST_LIST, FETCH_POST } from './mutations-types';
 
 export default {
 	[FETCH_POST_LIST](state, posts) {
 		state.posts = posts;
+	},
+	[FETCH_POST](state, post) {
+		state.post = post;
 	},
 };
