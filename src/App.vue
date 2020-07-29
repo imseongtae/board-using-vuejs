7161eb7e63b431869efe0773d86088323b940fc9
imseongtae <dev.haemil@gmail.com>
2020-07-29 23:37:00 +0900

    feat: Add named Router-view for header component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
 	<div id="app">
-		<!-- main -->
+		<!-- header라는 이름의 router-view -->
+		<!-- 이름을 부여한 RouterView 컴포넌트는 원하는 컴포넌트를 렌더할 수 있음 -->
+		<router-view name="header" />
+		<!-- main | 처럼 이름이 없으면 default 라는 name을 부여 받는다. -->
 		<router-view />
 	</div>
 </template>
+
+<script>
+export default {
+	name: 'App',
+};
+</script>
 
 <style lang="scss">
 // style 적용
