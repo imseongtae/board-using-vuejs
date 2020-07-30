ae8b55e54122f1618999889cd2e94725d7a42dc6
imseongtae <dev.haemil@gmail.com>
2020-07-30 23:14:32 +0900

    feat: Add feature for update and delete
    - Add Update post router link
    - Add feature for delete post

--- src/pages/PostViewPage.vue
+++ src/pages/PostViewPage.vue
@@ -5,6 +5,10 @@
 		<!-- 이를 위해 v-if 디렉티브 할당 -->
 		<post-view v-if="post" :post="post" />
 		<p v-else>게시글 불러오는 중</p>
+		<router-link :to="{ name: 'PostEditPage', params: { postId } }"
+			>수정</router-link
+		>
+		<button @click="onDelete">삭제</button>
 		<router-link :to="{ name: 'PostListPage' }">목록</router-link>
 	</div>
 </template>
@@ -13,6 +17,8 @@
 import { mapState, mapActions } from 'vuex';
 
 import PostView from '@/components/PostView.vue';
+
+import api from '@/api';
 
 export default {
 	name: 'PostViewPage',
@@ -40,6 +46,30 @@ export default {
 	methods: {
 		// 헬퍼함수를 컴포넌트 메소드에 매핑
 		...mapActions(['fetchPost']),
+		onDelete() {
+			// mapState 헬퍼함수를 통해 매핑된 post의 id 값을 변수에 할당
+			const { id } = this.post;
+			console.log(id);
+			api
+				.delete(`/posts/${id}`)
+				.then(() => {
+					// 게시글 삭제 성공 시, 성공에 대한 메시지 노출
+					alert('게시물이 삭제됨');
+					// 더 이상 노출된 메시지가 없으므로 리스트로 이동
+					this.$router.push({ name: 'PostListPage' });
+				})
+				.catch(err => {
+					if (err.response.status === 401) {
+						// HTTP 상태코드가 401인 경우 로그인 안내 메시지 출력
+						alert('로그인이 필요합니다.');
+						// 로그인 페이지로 이동
+						this.$router.push({ name: 'Signin' });
+					} else {
+						// HTTP 상태코드가 401이 아닌 경우 서버에서 내려준 메시지를 노출
+						alert(err.response.data.msg);
+					}
+				});
+		},
 	},
 };
 </script>
