fa7dba44b4bba604f6f75ae6c9fe199502b20c87
imseongtae <dev.haemil@gmail.com>
2020-07-31 14:17:22 +0900

    feat: Add CommentForm component for comment feature

---
+++ src/components/CommentForm.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="comment-form">
+		<textarea
+			v-model="comment"
+			rows="5"
+			placeholder="댓글을 입력해주세요."
+		></textarea>
+		<button @click="onSubmit">등록</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CommentForm',
+	data() {
+		return {
+			comment: '',
+		};
+	},
+	methods: {
+		onSubmit() {
+			const { comment } = this;
+			this.$emit('submit', comment);
+			// 댓글 작성 후 입력 데이터 초기화
+			this.comment = '';
+		},
+	},
+};
+</script>
+
+<style></style>
