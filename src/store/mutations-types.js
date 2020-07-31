99860f609206de2778225637733b3c6d0a6e1af6
imseongtae <dev.haemil@gmail.com>
2020-07-31 14:20:45 +0900

    feat: Add mutations-types
    - Add mutation type for create comment
    - Add mutation type for modify comment
    - Add mutation type for delete comment

--- src/store/mutations-types.js
+++ src/store/mutations-types.js
@@ -9,3 +9,12 @@ export const SET_MY_INFO = 'SET_MY_INFO';
 // 사용자 인증과 관련된 데이터를 제거하는 변이 타입 추가
 export const DESTROY_ACCESS_TOKEN = 'DESTROY_ACCESS_TOKEN';
 export const DESTROY_MY_INFO = 'DESTROY_MY_INFO';
+
+// 댓글을 생성하기 위한 변이 타입 추가
+export const UPDATE_COMMENT = 'UPDATE_COMMENT';
+
+// 댓글 상태를 수정하기 위한 변이 타입 추가
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+
+// 댓글을 삭제하기 위한 변이의 타입 선언
+export const DELETE_COMMENT = 'DELETE_COMMENT';
